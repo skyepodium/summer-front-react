5b8979f34541702599a4cab0c810082d6d25eae1
skyepodium <skyepodium@gmail.com>
2019-08-15 15:55:55 +0900

    add backgroundImage

--- src/layouts/TheTitle.js
+++ src/layouts/TheTitle.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import './TheTitle.css'
+import image from '../assets/summer.jpg'
+
+let style = {
+    backgroundImage: `url(${image})`
+}
 
 export default class TheTitle extends Component {
     render() {
         return (
             <div
                 className="TheTitleWrapper" 
+                style={style}
             />
         )
     }
